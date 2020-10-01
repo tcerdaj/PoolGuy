71ba959188c88b797ecd0a3277c8d09240d394fe
Teofilo Cerda <tcerda@fieldedge.com>
2020-09-30 23:54:04 -0400

    Adding materials

---
+++ PoolGuy.Mobile/PoolGuy.Mobile/PoolGuy.Mobile/ViewModels/HomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PoolGuy.Mobile.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public HomeViewModel()
+        {
+            Title = this.GetType().Name.Replace("ViewModel", "");
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
