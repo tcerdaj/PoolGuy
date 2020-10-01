41bb775e5045ed0b92faf6f44ea65cc2f2599e6c
Teofilo Cerda <tcerda@fieldedge.com>
2020-09-30 21:44:08 -0400

    *Initial commit

---
+++ PoolGuy.Mobile/PoolGuy.Mobile/PoolGuy.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using PoolGuy.Mobile.ViewModels;
+
+namespace PoolGuy.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ PoolGuy.Mobile/PoolGuy.Mobile/PoolGuy.Mobile/Views/ItemsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PoolGuy.Mobile.Models;
+using PoolGuy.Mobile.Views;
+using PoolGuy.Mobile.ViewModels;
+
+namespace PoolGuy.Mobile.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
