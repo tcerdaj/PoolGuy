504448a5477e7bc1b89fd1d21ae0c847fa303599
Teofilo Cerda <tcerda@fieldedge.com>
2020-11-23 02:37:44 -0500

    Addding data structure.

--- PoolGuy.Mobile.Data/Models/WeatherHistoryModel.cs
+++ PoolGuy.Mobile.Data/Models/WeatherHistoryModel.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using SQLiteNetExtensions.Attributes;
 using System;
 
 namespace PoolGuy.Mobile.Models
@@ -11,8 +12,13 @@ namespace PoolGuy.Mobile.Models
         public int lon { get; set; }
         public string timezone { get; set; }
         public int timezone_offset { get; set; }
+        [ForeignKey(typeof(Current))]
+        public int CurrentId { get; set; }
+        [OneToOne(CascadeOperations = CascadeOperation.All)]
         public Current current { get; set; }
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
         public Daily[] daily { get; set; }
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
         public Alert[] alerts { get; set; }
     }
 
@@ -31,26 +37,40 @@ namespace PoolGuy.Mobile.Models
         public int visibility { get; set; }
         public float wind_speed { get; set; }
         public int wind_deg { get; set; }
-        public Weather[] weather { get; set; }
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public WeatherModel[] weather { get; set; }
     }
 
-    public class Daily 
+    public class Daily
     {
         public int dt { get; set; }
         public int sunrise { get; set; }
         public int sunset { get; set; }
+        [ForeignKey(typeof(Temp))]
+        public Guid TempId { get; set; }
+        
+        [OneToOne(CascadeOperations = CascadeOperation.All)]
         public Temp temp { get; set; }
+        
+        [ForeignKey(typeof(Feels_Like))]
+        public Guid FeelsLikeId { get; set; }
+
+        [OneToOne(CascadeOperations = CascadeOperation.All)]
         public Feels_Like feels_like { get; set; }
         public int pressure { get; set; }
         public int humidity { get; set; }
         public float dew_point { get; set; }
         public float wind_speed { get; set; }
         public int wind_deg { get; set; }
-        public Weather[] weather { get; set; }
+        [OneToMany(CascadeOperations = CascadeOperation.All)]
+        public WeatherModel[] weather { get; set; }
         public int clouds { get; set; }
         public float pop { get; set; }
         public float uvi { get; set; }
         public float rain { get; set; }
+        [ForeignKey(typeof(Temp))]
+        public Guid RainId { get; set; }
+        [OneToOne(CascadeOperations = CascadeOperation.All)]
         public Rain Rain { get; set; }
     }
 
