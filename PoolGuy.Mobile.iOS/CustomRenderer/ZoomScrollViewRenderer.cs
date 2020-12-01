82a25eb291a9b703ad15793e2d624ab0a7ef48f9
Teofilo Cerda <tcerda@fieldedge.com>
2020-12-01 18:31:13 -0500

    Adding zoomed scrollview.

---
+++ PoolGuy.Mobile.iOS/CustomRenderer/ZoomScrollViewRenderer.cs
@@ -0,0 +1,38 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.Linq;
+using PoolGuy.Mobile.iOS.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(ScrollView), typeof(ZoomScrollViewRenderer))]
+namespace PoolGuy.Mobile.iOS.CustomRenderer
+{
+    public class ZoomScrollViewRenderer : ScrollViewRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            MaximumZoomScale = 3f;
+            MinimumZoomScale = 1.0f;
+
+        }
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+
+            if (Subviews.Length > 0)
+            {
+                ViewForZoomingInScrollView += GetViewForZooming;
+            }
+            else
+            {
+                ViewForZoomingInScrollView -= GetViewForZooming;
+            }
+
+        }
+        public UIView GetViewForZooming(UIScrollView sv)
+        {
+            return this.Subviews.FirstOrDefault();
+        }
+    }
+}
