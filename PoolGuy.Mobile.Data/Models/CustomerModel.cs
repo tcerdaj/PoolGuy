19473332a3931d1ebf3dfc7a5a9c59f6292b2968
Teofilo Cerda <tcerdaj@hotmail.com>
2022-04-07 11:04:20 -0400

    Removing stopid

--- PoolGuy.Mobile.Data/Models/CustomerModel.cs
+++ PoolGuy.Mobile.Data/Models/CustomerModel.cs
@@ -114,16 +114,6 @@ namespace PoolGuy.Mobile.Data.Models
             set { _active = value; OnPropertyChanged("Active"); }
         }
 
-        //[ForeignKey(typeof(StopModel))]
-        public Guid StopId { get; set; }
-
-        //private StopModel _currentStop;
-        //[OneToOne(CascadeOperations = CascadeOperation.All)]
-        //public StopModel CurrentStop
-        //{
-        //    get { return _currentStop; }
-        //    set { _currentStop = value; OnPropertyChanged("CurrentStop"); }
-        //}
 
         WorkStatus _status;
         public WorkStatus Status
